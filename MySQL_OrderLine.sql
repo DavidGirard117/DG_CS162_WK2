8c9211a8e7d28e54674126ad1d5c8aa67a4e91ce
DavidGirard117 <56056431+DavidGirard117@users.noreply.github.com>
2020-04-20 20:43:26 -0700

    Update MySQL_OrderLine.sql

--- MySQL_OrderLine.sql
+++ MySQL_OrderLine.sql
@@ -1,11 +1,19 @@
 
 CREATE TABLE OrderLine (
 OrderNumber INT NOT NULL, FOREIGN KEY (OrderNumber) REFERENCES Orders(ID),
-ProductNumber INT NOT NULL, FOREIGN KEY (ProductNumber) REFERENCES Product(ID),
+ProductNumber VARCHAR(45) NOT NULL, FOREIGN KEY (ProductNumber) REFERENCES Product(ID),
 Quantity INT NOT NULL, PRIMARY KEY (OrderNumber, ProductNumber)
 );
 
 
+
+
+
+/*
+1234	A123	100
+1234	B234	200
+1234	C345	5
+*/
 
 
 

2e7c89b6532312e75ba778536af01b86ab01fa6e
DavidGirard117 <56056431+DavidGirard117@users.noreply.github.com>
2020-04-20 20:29:35 -0700

    Update MySQL_Orders.sql

--- MySQL_Orders.sql
+++ MySQL_Orders.sql
@@ -1,10 +1,16 @@
 
 USE order_form;
 CREATE TABLE Orders (
-ID INT PRIMARY KEY,
+ID INT NOT NULL PRIMARY KEY,
 CustomerNumber INT, FOREIGN KEY (CustomerNumber) REFERENCES Customer(ID),
 OrderDate DATE
 );
+
+
+
+
+//1234	9876	1998-11-04
+		
 
 
 

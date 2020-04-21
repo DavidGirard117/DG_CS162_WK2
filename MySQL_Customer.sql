a39a35256aeb31cc90477233b3e098717622e857
DavidGirard117 <56056431+DavidGirard117@users.noreply.github.com>
2020-04-20 20:25:08 -0700

    Update MySQL_Customer.sql

--- MySQL_Customer.sql
+++ MySQL_Customer.sql
@@ -3,8 +3,7 @@ USE order_form;
 CREATE TABLE Customer (
 ID INT NOT NULL PRIMARY KEY,
 CustomerName VARCHAR(45) NOT NULL,
-OrderDate DATE NOT NULL
-);
+Address VARCHAR(45) NOT NULL;
 
 //9876	Billy	456 High Tower Street, Hong Kong China
 		
